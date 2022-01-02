1ae87cfdc3b06635571db9f4b1fdc5588412a7c1
Gothmate <gothmate@gmail.com>
2022-01-01 22:58:28 -0300

    animação menu hamburguer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const selection = document.querySelectorAll('.select')
-
 const active = document.querySelector('.active')
+const modal = document.getElementById('modal')
+const hamburguer = document.querySelector('.hamburguer')
+const animate = document.querySelector('.animate')
 
 selection.forEach(el => {
   el.addEventListener('click', () => {
@@ -18,4 +20,17 @@ function selectItem(el) {
   if (el.classList.value == 'active') {
     el.classList.remove('active')
   }
+}
+
+hamburguer.addEventListener('click', () => {
+  hamburguer.classList.add('animate')
+  hamburguer.classList.remove('hamburguer')
+  modal.style.opacity = 1
+})
+
+if (animate) {
+  modal.addEventListener('click', () => {
+    animate.classList.add('hamburguer')
+    animate.classList.remove('animate')
+  })
 }
